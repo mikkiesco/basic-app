17fd7181f36f5624cdcce7578956c15dc2a5e27e
Paul Mikki <ikkdotim@gmail.com>
2022-06-28 22:16:16 +0800

    Added Authentication Controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Validator;
+use Auth;
+
+class AuthController extends Controller
+{
+    public function attempt(Request $request){
+        $validator = Validator::make($request->all(), [
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+ 
+        if ($validator->fails()) {
+            return abort(403);
+        }
+
+        if (Auth::attempt(['username' => $request->get('username'), 'password' => $request->get('password')])) {
+            return redirect('/');
+        }
+    }
+}
